e3b44e1e8dc40e989c80838a62248baf654450b5
ME <mebener44@gmail.com>
2021-08-21 18:19:16 -0700

    updated models and index

--- models/Product.js
+++ models/Product.js
@@ -15,11 +15,26 @@ Product.init(
       primaryKey: true,
       autoIncrement: true
     },
-    category_name: {
+    product_name: {
       type: DataTypes.STRING,
       allowNull: false
     },
-    
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+    }
+  }
+
 
   },
   {
--- models/index.js
+++ models/index.js
@@ -5,12 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Categories, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+});
 
-// Categories have many Products
+// Categories have many Products ??
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tags, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+
+})
+
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+
+})
+
 
 module.exports = {
   Product,
